1784d8d8b9d2c909509a10ee350c6dc73044e7ee
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-08-21 19:49:23 -0300

    atualizações

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -28,6 +28,36 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo "<script>alert('Erro ao cadastrar usuário.');</script>";
     }
 }
+// Obtendo o nome do perfil do usuario logado 
+$id_perfil = $_SESSION['perfil'];
+$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil->bindParam(':id_perfil', $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+
+$permissoes = [
+    1=> ["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
+        "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+        "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+        "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]],
+
+    2=> ["Cadastrar"=>["cadastro_cliente.php"],
+        "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php", "alterar_fornecedor.php"]],
+        
+    3=> ["Cadastrar"=>[ "cadastro_fornecedor.php", "cadastro_produto.php"],
+        "Buscar"=>[ "buscar_cliente.php", "buscar_fornecedor.php", "buscar_funcionario.php"],
+        "Alterar"=>[ "alterar_fornecedor.php", "alterar_produto.php"],
+        "Excluir"=>["excluir_produto.php"]],
+    
+    4=> ["Cadastrar"=>[ "cadastro_cliente.php"],
+        "Alterar"=>[ "alterar_cliente.php"]]
+];
+
+$opcoes_menu = $permissoes[$id_perfil]; 
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -38,6 +68,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+<nav>
+        <ul class="menu">
+            <?php foreach($opcoes_menu as $categoria=>$arquivos): ?>
+            <li class="dropdown">
+                    <a href="#"><?php echo $categoria; ?></a>
+                    <ul class="dropdown-menu">
+                    <?php foreach($arquivos as $arquivo):?>
+                        <li>
+                            <a href="<?=$arquivo ?>"> <?= ucfirst(str_replace("_", " ",basename ($arquivo, ".php")))?></a>
+                        </li>
+                        <?php endforeach; ?> 
+                    </ul>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+     </nav>
+
     <h2> Cadastrar Usuário </h2>
     <form action="cadastro_usuario.php" method="POST">
         <label for="nome"> Nome: </label>
