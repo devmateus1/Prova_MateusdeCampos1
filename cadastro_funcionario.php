7d08300ce478a168cda421ebcbb7dcab20a8b3df
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-08-26 19:43:46 -0300

    criação do cadastrar funcionario

---
+++ cadastro_funcionario.php
@@ -0,0 +1,143 @@
+<?php 
+session_start()
+require_once 'conexao.php';
+
+
+if ($_SESSION['perfil'] != 1) {
+    echo "Acesso Negado"
+    exit();
+}
+
+$id_perfil = $_SESSION['perfil'];
+$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil->bindParam(':id_perfil', $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+
+$permissoes = [
+    1=> ["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"],
+        "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+        "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+        "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"]],
+
+    2=> ["Cadastrar"=>["cadastro_cliente.php"],
+        "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+        "Alterar"=>["alterar_cliente.php", "alterar_fornecedor.php"]],
+        
+    3=> ["Cadastrar"=>[ "cadastro_fornecedor.php", "cadastro_produto.php"],
+        "Buscar"=>[ "buscar_cliente.php", "buscar_fornecedor.php", "buscar_funcionario.php"],
+        "Alterar"=>[ "alterar_fornecedor.php", "alterar_produto.php"],
+        "Excluir"=>["excluir_produto.php"]],
+    
+    4=> ["Cadastrar"=>[ "cadastro_cliente.php"],
+        "Alterar"=>[ "alterar_cliente.php"]]
+];
+
+$opcoes_menu = $permissoes[$id_perfil]; 
+
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $nome_funcionario = $_POST['nome_funcionario'];
+    $endereco = $_POST['endereco'];
+    $telefone = $_POST['telefone'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $datanascimento = $_POST['datanascimento'];
+
+    $sql = "INSERT INTO funcionario (nome_funcionario, endereco, telefone, email, cpf, datanascimento ) VALUES (:nome_funcionario, :endereco, :email, :cpf, :datanascimento)";
+    $stmt = $pdo->prepare($sqlPerfil);
+    $stmt->bindParam(':nome_funcionario', $nome_funcionario);
+    $stmt->bindParam(':endereco', $endereco);
+    $stmt->bindParam(':telefone', $telefone);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':cpf', $cpf);
+    $stmt->bindParam(':datanascimento', $datanascimento);
+
+    if ( $stmt->execute()){
+        echo "<script>alert('Funcionário cadastrado com sucesso!');</script>";
+    }else {
+        echo "<script>alert('Erro ao cadastrar Funcionário.');</script>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Funcionario</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<nav>
+        <ul class="menu">
+            <?php foreach($opcoes_menu as $categoria=>$arquivos): ?>
+            <li class="dropdown">
+                    <a href="#"><?php echo $categoria; ?></a>
+                    <ul class="dropdown-menu">
+                    <?php foreach($arquivos as $arquivo):?>
+                        <li>
+                            <a href="<?=$arquivo ?>"> <?= ucfirst(str_replace("_", " ",basename ($arquivo, ".php")))?></a>
+                        </li>
+                        <?php endforeach; ?> 
+                    </ul>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+     </nav>
+     <br>
+
+     <h2> Cadastrar Funcionário </h2>
+
+     <form action="cadastrar_funcionario.php" method="POST">
+        <label for="nome_funcionario"> Nome do funcionário: </label>
+        <input type="text" id="nome_funcionario" name="nome_funcionario" required onkeyup="validarNome()">
+
+        <label for="endereco"> Endereço: </label>
+        <input type="text" name="endereco" id="endereco" required>
+
+        <label for="telefone"> Telefone: </label>
+        <input type="text" name="telefone" id="telefone" required>
+
+        <label for="email"> Email: </label>
+        <input type="Email" name="email" id="email" required>
+
+        <label for="cpf"> Cpf: </label>
+        <input type="text" name="cpf" id="cpf" required>
+
+        <label for="datanascimento"> Cpf: </label>
+        <input type="text" name="datanascimento" id="datanascimento" required>
+
+        <label for="id_perfil"> Perfil: </label>
+       
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+        <button type="reset" class="btn btn-primary">Cancelar</button>
+        
+    </form>
+
+   <center> <a href="principal.php" class="btn btn-primary">Voltar</a></center> 
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
